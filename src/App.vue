54e28af56dad9cbf4e66a02235ec8c84d61570ba
Nikolay Zverev <user@mail.ru>
2021-09-28 00:06:36 +0600

    Created project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+* {
+  margin: 0;
+}
+body {
+  background: rgba(255, 254, 251, 0.8);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="container"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss">
+  .container {
+    width: 1440px;
+    padding: 0 17px;
+    margin: auto;
+  }
+</style>
